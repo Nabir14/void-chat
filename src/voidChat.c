4500443e778acc3c555f31e28be7bcbc0686b267
Nabir14 <mushfiquefarhannabir@gmail.com>
2025-09-16 00:37:29 +0600

    buffer clearing fix, input update fix

--- src/voidChat.c
+++ src/voidChat.c
@@ -9,17 +9,23 @@
 #define VERSION 1.0
 #define AUTHOR "Nabir14"
 
+char alias[8];	
 struct serverInfo server;
 struct clientInfo client;
 
 void appendClient(struct sockaddr_in *clientList, int *clientCount){
 	if(*clientCount > 0){
-	for(int i = 0; i < *clientCount; i++){
-		if(!(server.cAddr.sin_addr.s_addr == clientList[i].sin_addr.s_addr && server.cAddr.sin_port == clientList[i].sin_port)){
-			clientList[*clientCount] = server.cAddr;
-			(*clientCount)++;
+		for(int i = 0; i < *clientCount; i++){
+			// Check If Client Doesnt Exists
+			// like: Server Recived Client Addr == Addr of Client at index (i)
+			// and: Server Recived Client Port == Port of Client at index (i)
+			if(!(server.cAddr.sin_addr.s_addr == clientList[i].sin_addr.s_addr && server.cAddr.sin_port == clientList[i].sin_port)){
+				if(*clientCount < 16){
+					clientList[*clientCount] = server.cAddr;
+					(*clientCount)++;
+				}
+			}
 		}
-	}
 	}else{
 		clientList[0] = server.cAddr;
 		(*clientCount)++;
@@ -31,10 +37,16 @@ void *serverRead(){
 	struct sockaddr_in clients[16];
 	int connectedClients = 0;
 	while(true){
+		// Clear Previous Buffer
 		memset(msg, 0, sizeof(msg));
+
+		// Read Messages From Client
 		receiveStrServer(&server, msg, sizeof(msg));
+
 		printf("[LOG]: %s\n", msg);
 		appendClient(clients, &connectedClients);
+
+		// Echo Back To All Connected Clients
 		for(int i = 0; i < connectedClients; i++){
 			sendStrServer(&server, msg, strlen(msg), &clients[i]);
 		}
@@ -45,9 +57,19 @@ void *serverRead(){
 void *clientRead(){
 	char msg[256];
 	while(true){
+		// Clear Previous Buffer
 		memset(msg, 0, sizeof(msg));
+		
+		// Read Messages From Server
 		receiveStrClient(&client, msg, sizeof(msg));
-		printf("\n%s\n", msg);
+		// Remove: Current Line (Input)
+		// Print: The Message
+		// Note: This actually moves the input feild down.
+		printf("\r%s\n", msg);
+		printf("[%s]: ", alias);
+
+		// Force Update Terminal Screen
+		fflush(stdout);
 	}
 	return NULL;
 }
@@ -74,34 +96,43 @@ void runServer(){
 }
 
 void runClient(){
-	char alias[8];	
+
+	// Define Function Vars
 	char msg[256];
 	bool connected = true;
 	
-	// Get Info
+	// Get Server Info
 	printf("IP: ");
 	scanf("%[^\n]", client.ip);
 	getchar();
 
+	// Get User Info
 	printf("Alias: ");
 	scanf("%[^\n]", alias);
 	getchar();
 
+	// Set Default Port
 	client.port = DEFAULT_PORT;
 
 	// Initialize Client
 	clientInit(&client);	
 
+	// Start Reading Messages From Server (Multi-Thread)
 	pthread_t clientThread;
 	pthread_create(&clientThread, NULL, clientRead, NULL);
+	
+	// Print Input Label ([alias]:)
+	printf("[%s]: ", alias);
+
+	// Handle Messages
 	while(connected){
-		// Clear Buffer
+		// Clear Previous Buffer
 		memset(msg, 0, sizeof(msg));
 
 		// Scan Message
-		printf("$[%s]: ", alias);
 		scanf("%[^\n]", msg);
 		getchar();
+
 		if(!strcmp(msg, "/exit")){
 			// Disconnect
 			pthread_cancel(clientThread);
@@ -110,8 +141,8 @@ void runClient(){
 			// Log
 			printf("[LOG]: Disconnected From Server!\n");
 		}else{
+			// Define And Send Message
 			char message[256];
-			memset(message, 0, sizeof(message));
 			strcpy(message, alias); // user
 			strcat(message, ": ");   // user:
 			strcat(message, msg);   // user: msg
